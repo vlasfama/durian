9f1df28b349fa6386f91d81ff68ca413bfbe0678
Nagaraj shetty <nagarajmanjunath22@gmail.com>
2020-01-15 19:18:51 +0800

    added rpc function

--- cli/src/rpc.rs
+++ cli/src/rpc.rs
@@ -1,24 +1,41 @@
-use jsonrpc_core::Result;
+use jsonrpc_core_client::transports::local;
+use jsonrpc_core::futures::future::{self, Future, FutureResult};
+use jsonrpc_core::{Error, IoHandler, Result};
 use jsonrpc_derive::rpc;
 
+/// Rpc trait
 #[rpc]
 pub trait Rpc {
+	/// Returns a protocol version
+	#[rpc(name = "protocolVersion")]
+	fn protocol_version(&self) -> Result<String>;
+
 	/// Adds two numbers and returns a result
-	#[rpc(name = "add")]
-	fn add(&self, a:u64, b:u64) -> Result<u64>;
+	#[rpc(name = "add", alias("callAsyncMetaAlias"))]
+	fn add(&self, a: u64, b: u64) -> Result<u64>;
+
+	/// Performs asynchronous operation
+	#[rpc(name = "callAsync")]
+	fn call(&self, a: u64) -> FutureResult<String, Error>;
 }
 
-pub struct RpcImpl;
+struct RpcImpl;
 
 impl Rpc for RpcImpl {
+	fn protocol_version(&self) -> Result<String> {
+		Ok("version1".into())
+	}
+
 	fn add(&self, a: u64, b: u64) -> Result<u64> {
 		Ok(a + b)
+	}
+
+	fn call(&self, _: u64) -> FutureResult<String, Error> {
+		future::ok("OK".to_owned())
 	}
 }
 
 pub fn rpc_init() {
-	let mut io = jsonrpc_core::IoHandler::new();
-	io.extend_with(RpcImpl.to_delegate())
+	let mut io = IoHandler::new();
+	io.extend_with(RpcImpl.to_delegate());
 }
-
-
