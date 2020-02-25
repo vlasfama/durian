a9939511150f8d7d3d74e2b92f721b8600898cc4
Nagaraj shetty <nagarajmanjunath22@gmail.com>
2020-02-25 15:21:56 +0800

    Added new module for rpc

--- cli/src/mod.rs
+++ cli/src/mod.rs
@@ -1,5 +1,4 @@
-mod event_loop;
+
 mod rpc;
-pub use event_loop::{event_loop, forever};
 pub use rpc;
 
--- cli/src/rpc.rs
+++ cli/src/rpc.rs
@@ -1,87 +1,24 @@
-// use jsonrpc_http_server::jsonrpc_core::*;
-// use jsonrpc_http_server::*;
+extern crate jsonrpc_http_server;
+use jsonrpc_http_server::jsonrpc_core::*;
+use jsonrpc_http_server::{AccessControlAllowOrigin, DomainsValidation, ServerBuilder};
 
-// pub fn InitRPC() {
-// 	let mut io = IoHandler::default();
-// 	io.add_method("gas_Price", |_| {
-// 		println!("hhhhh");
-// 		Ok(Value::String("Hello World!".into()))
-// 	});
-// 	io.add_method("eth_Transaction", |_| {
-// 		println!("hhhhh");
-// 		Ok(Value::String("Hello World!".into()))
-// 	});
+pub fn initRPC() {
+	let mut io = IoHandler::default();
+	io.add_method("eth_gasPrice", |_params| {
+		futures::finished(Value::String("0".to_owned()))
+	});
 
-// 	let server = ServerBuilder::new(io)
-// 		.cors(DomainsValidation::AllowOnly(vec![
-// 			AccessControlAllowOrigin::Null,
-// 		]))
-// 		.start_http(&"127.0.0.1:3030".parse().unwrap())
-// 		.expect("Unable to start RPC server");
+	io.add_method("eth_sendTransaction", |_params| {
+		println!("{:?}", _params);
+		futures::finished(Value::String("0".to_owned()))
+	});
 
-// 	server.wait();
-// }
+	let server = ServerBuilder::new(io)
+		.cors(DomainsValidation::AllowOnly(vec![
+			AccessControlAllowOrigin::Null,
+		]))
+		.start_http(&"127.0.0.1:8545".parse().unwrap())
+		.expect("Unable to start RPC server");
 
-#![deny(warnings)]
-extern crate futures;
-extern crate hyper;
-extern crate pretty_env_logger;
-
-use hyper::header::ContentLength;
-use hyper::server::{Request, Response, Service};
-use hyper::{Get, Post, StatusCode};
-use hyper::server::Http
-
-static INDEX: &'static [u8] = b"Try POST /echo";
-
-#[derive(Clone, Copy)]
-struct Echo;
-
-impl Service for Echo {
-	type Request = Request;
-	type Response = Response;
-	type Error = hyper::Error;
-	type Future = ::futures::Finished<Response, hyper::Error>;
-
-	fn call(&self, req: Request) -> Self::Future {
-		::futures::finished(match (req.method(), req.path()) {
-			(&Get, "/") | (&Get, "/echo") => Response::new()
-				.with_header(ContentLength(INDEX.len() as u64))
-				.with_body(INDEX),
-			(&Post, "/echo") => {
-				let mut res = Response::new();
-				if let Some(len) = req.headers().get::<ContentLength>() {
-					res.headers_mut().set(len.clone());
-				}
-				res.with_body(req.body())
-			}
-			(&Post, "/get_gasPrice") => {
-				let mut res = Response::new();
-				if let Some(len) = req.headers().get::<ContentLength>() {
-					res.headers_mut().set(len.clone());
-				}
-				res.with_body(req.body())
-			}
-			(&Post, "/eth_sendTransaction") => {
-				let mut res = Response::new();
-				if let Some(len) = req.headers().get::<ContentLength>() {
-					res.headers_mut().set(len.clone());
-				}
-				res.with_body(req.body())
-			}
-			_ => Response::new().with_status(StatusCode::NotFound),
-		})
-	}
-}
-
-pub fn InitRPC() {
-	pretty_env_logger::init().unwrap();
-	let addr = "127.0.0.1:1337".parse().unwrap();
-
-	let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
-	println!(
-		"Listening on http://{} with 1 thread.",
-		server.local_addr().unwrap()
-	);
-	server.run().unwrap();
+	server.wait();
 }
